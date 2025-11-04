cde41da83be447570d260ee7f5200730902ad133
Ronaldo Chan <ronaldopchan@outlook.com>
2025-11-04 18:56:25 +0800

    Update

--- http_demo.go
+++ http_demo.go
@@ -1,4 +1,4 @@
-package http_demo
+package main
 
 import (
 	"encoding/json"
@@ -11,33 +11,42 @@ import (
 )
 
 func main() {
-	// CLI flags for IP and port
+	// CLI flags
 	ip := flag.String("ip", "0.0.0.0", "IP address to bind to")
-	port := flag.Int("port", 0, "Port to listen on (if 0, a random port between 50000–60000 will be used)")
+	port := flag.Int("port", 0, "Port to listen on (if 0, random 50000–60000)")
 	flag.Parse()
 
-	// Seed RNG
+	// Randomize port if not specified
 	rand.Seed(time.Now().UnixNano())
-
-	// If no port provided, generate a random one between 50000–60000
 	if *port == 0 {
 		*port = rand.Intn(10001) + 50000 // 50000–60000 inclusive
 	}
 
-	// Handler for "/"
+	// Define HTTP handler
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		// Prepare response
 		response := map[string]string{"message": "OK Success"}
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
-		if err := json.NewEncoder(w).Encode(response); err != nil {
-			log.Println("Error encoding JSON:", err)
-		}
+		_ = json.NewEncoder(w).Encode(response)
+
+		// Log request
+		duration := time.Since(start)
+		clientIP := r.RemoteAddr
+		log.Printf("[%s] %s %s from %s (%v)",
+			time.Now().Format("2006-01-02 15:04:05"),
+			r.Method,
+			r.URL.Path,
+			clientIP,
+			duration)
 	})
 
 	addr := fmt.Sprintf("%s:%d", *ip, *port)
 	log.Printf("Server listening on http://%s/", addr)
 
-	// Start the HTTP server
+	// Start HTTP server
 	if err := http.ListenAndServe(addr, nil); err != nil {
 		log.Fatalf("Failed to start server: %v", err)
 	}
